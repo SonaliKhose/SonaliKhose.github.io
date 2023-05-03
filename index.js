28443491e39537e37740ed6aaaa7c30a94375e44
SonaliKhose <sonalikhose600@gmail.com>
2023-05-04 00:15:41 +0530

    Doing

--- index.js
+++ index.js
@@ -1,31 +1,12 @@
-$(document).ready(function() {
-    $('#contact-form').on('submit', function(e) {
-      e.preventDefault();
-      // Send form data using AJAX
-      $.ajax({
-        url: $(this).attr('action'),
-        method: $(this).attr('method'),
-        data: $(this).serialize(),
-        success: function(response) {
-          // Show success message
-          $('#contact-form').html('<p class="success-message">Your message has been sent!</p>');
-        }
-      });
-    });
-  });
-  $(document).ready(function() {
-    $('#contact-form').on('submit', function(e) {
-      e.preventDefault();
-      // Send form data using AJAX
-      $.ajax({
-        url: $(this).attr('action'),
-        method: $(this).attr('method'),
-        data: $(this).serialize(),
-        success: function(response) {
-          // Show success message
-          $('#contact-form').html('<p class="success-message">Your message has been sent!</p>');
-        }
-      });
-    });
-  });
-    
+const form = document.getElementById("contact-form");
+
+form.addEventListener("click", function(event) {
+  event.preventDefault();
+  
+ alert("Your message sent successfully!!")
+  // Display a success message to the user
+ 
+  // Clear form inputs
+ 
+});
+form.reset();
